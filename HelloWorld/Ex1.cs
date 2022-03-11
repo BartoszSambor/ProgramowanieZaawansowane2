37cf0ef285921814b73cfde2893b19e0a8f8e4d9
Bartosz Sambór <72545684+BartoszSambor@users.noreply.github.com>
2022-03-11 14:11:46 +0100

    Add files via upload

---
+++ HelloWorld/Ex1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Ex1
+    {
+        public Ex1(int year)
+        {
+            Console.WriteLine("Exercise1");
+            if (year % 400 == 0  || year % 4 == 0 && year % 100 != 0)
+            {
+                Console.WriteLine("Przestępny");
+            } else
+            {
+                Console.WriteLine("Nie przestępny");
+            }
+        }
+    }
+}
---
+++ HelloWorld/Ex2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Ex2
+    {
+        public Ex2()
+        {
+            Console.WriteLine("Exercise2");
+            int input =int.Parse(Console.ReadLine());
+            int sum = 0;
+            while(input > 0)
+            {
+                sum += input % 10;
+                input /= 10;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
